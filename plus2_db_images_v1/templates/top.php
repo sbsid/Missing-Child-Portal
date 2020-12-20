db00a98b1cf47499d166a5708331a568d5d0b071
Saba Siddiqua Sadique Ahemed Siddiqui <54209159+sbsid@users.noreply.github.com>
2020-12-20 20:41:54 +0530

    Add files via upload

---
+++ plus2_db_images_v1/templates/top.php
@@ -0,0 +1,23 @@
+
+<br></br><center><h1>Lost Child Data </h1></center>
+<nav class='navbar  navbar-dark ' style='background-color: #2C5197;'>
+  <!-- <a class='navbar-brand' href='#'>Your Logo</a>
+
+  <a class='nav-link' href='#'>Signup</a> <a class='nav-link' href='#'>Login</a> -->
+  
+
+  <a href="http://localhost/php/welcome.php" style="color:white;">Home</a>
+  <a href="http://localhost/php/index.php" style="color:white;">Entry </a>
+  <a href="http://localhost/plus2_db_images_v1/#" style="color:white;">View All Posts </a>
+  <a href="http://localhost/plus2_db_images_v1/my_post.php" style="color:white;">View My Posts </a>
+  <a href="http://localhost/plus2_db_images_v1/my_post.php" style="color:white;">Edit </a>
+  <a href="http://localhost/plus2_db_images_v1/my_post.php"style="color:white;">Delete </a>
+  
+  <a href="http://localhost/php/about.php" style="color:white;">About Us</a>
+  <a href="http://localhost/php/logout.php" style="color:white;"> Log Out</a>
+
+  
+  
+  
+  
+</nav><br></br>
